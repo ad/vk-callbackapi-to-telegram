e01982bb2b9ffa7ee01b55211dc66f136377fb79
Daniel Apatin <daniel@apatin.ru>
2024-02-05 14:27:50 +0300

    link to group

--- sender/messageNew.go
+++ sender/messageNew.go
@@ -8,11 +8,12 @@ import (
 
 func messageNew(m *models.VkCallbackRequest) string {
 	return fmt.Sprintf(
-		"<a href=\"https://vk.com/gim%d?sel=%d&msgid=%d\">Сообщение(#%d)</a> в группе от vk.com/id%d: %s",
+		"<a href=\"https://vk.com/gim%d?sel=%d&msgid=%d\">Сообщение(#%d)</a> в <a href=\"https://vk.com/gim%d\">группе</a> от vk.com/id%d: %s",
 		m.GroupID,
 		m.Object.Message.FromID,
 		m.Object.Message.ID,
 		m.Object.Message.ConversationMessageID,
+		m.GroupID,
 		m.Object.Message.FromID,
 		m.Object.Message.Text,
 	)
